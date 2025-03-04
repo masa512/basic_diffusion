6400f3be3907025c6c46e0f3cae730fdf8f1e77b
masa512 <ms4@illinois.edu>
2025-03-04 04:22:33 +0000

    Added requirements (blank), updated setup.py

--- dm/setup.py
+++ dm/setup.py
@@ -4,4 +4,10 @@ setup(
     name="dm",
     version="0.0.1",
     packages=find_packages(),  # Finds "project" automatically
+    install_requires=[
+        "numpy",  # Add dependencies here
+        "scipy",
+        "torch",
+        "torchaudio"
+    ]
 )
