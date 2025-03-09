a8d1adaf3e717fe0b3d802c6ea9ed7aeda4a0f10
Masayoshi <ms4@illinois.edu>
2025-03-09 06:25:43 +0000

    MNIST Denoise was a success

--- dm/dm/img_seg.py
+++ dm/dm/img_seg.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 def train(model,trainloader,optimizer,n_epochs,device = "cuda" if torch.cuda.is_available() else "cpu"):
@@ -43,8 +44,36 @@ def train(model,trainloader,optimizer,n_epochs,device = "cuda" if torch.cuda.is_
         print(f"Epoch {t}: Avg Loss - {epoch_loss/len(trainloader)}")
 
     
-    
+def test_example(model,dataset,device = "cuda" if torch.cuda.is_available else "cpu"):
 
+    # Load model to device
+    model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+
+        # Load random index
+        i = np.random.randint(len(dataset))
+
+        # Load the data
+        clean_img, noisy_img, label = dataset[i]
+        clean_img, noisy_img = clean_img.to(device), noisy_img.to(device)
+
+        # Estimate denoised image
+        x = noisy_img.unsqueeze(0)
+        pred = model(x).detach()
+
+        # Plot all three
+        f,ax = plt.subplots(1,3,figsize=(15,5))
+
+        ax[0].imshow(torch.squeeze(clean_img).cpu())
+        ax[0].axis('off')
+
+        ax[1].imshow(torch.squeeze(noisy_img).cpu())
+        ax[1].axis('off')
+
+        ax[2].imshow(torch.squeeze(pred).cpu())
+        ax[2].axis('off')
 
             
 
